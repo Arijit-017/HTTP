7f583b88a454d46d410880490eebd4d2f4517398
Arijit Ghosh <ajghosh017@gmail.com>
2024-08-30 20:22:20 +0530

    Add Rate Limiter

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+let count=0;
+function requestCounter(req, res, next) {
+  count++;
+  next();
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+function ratelimiter(req, res, next){
+  if(count>50){
+    res.send("You have reached the maximum number of request");
+  }
+  else{
+    next();
+  }
+};
+
+app.get('/',requestCounter ,ratelimiter , (req, res) => {
+  res.send(`The count is ${count}`);
 });
 
 app.listen(PORT, () => {
